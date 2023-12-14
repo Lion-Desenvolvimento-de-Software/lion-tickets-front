1bba69daffa37ec0ba31d2869057da85d9b1fd23
unknown <mauricio2017rubiale@gmail.com>
2023-12-14 13:09:12 -0300

    new component 'leyaoutCards'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <carousel />
+    <leyaout-cards />
   </div>
 </template>
 
 <script>
 import Carousel from '@/components/carousel.vue'
+import LeyaoutCards from '@/components/leyaoutCards.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    Carousel
+    Carousel,
+    LeyaoutCards
   }
 }
 </script>

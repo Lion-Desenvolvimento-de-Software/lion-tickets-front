5eeadac14343fb914a1de2d6b7c96d38ad444116
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-02-12 13:50:31 -0300

    Grid em coluna entre avatar e informações do usuáriol

---
+++ src/components/avatar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="box">
+    <label class="avatar">
+      <img src="../assets/images/R.png" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+
+}
+</script>
+
+<style scoped>
+* {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+.box {
+  position: relative;
+  width: 20em;
+  height: 20em;
+  top: 25px;
+  left: 25px;
+}
+.avatar::after {
+  opacity: 0;
+  color: #fff;
+  content: url("../assets/images/icons/photocamera.png");
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  background-color: rgba(0,0,0,0.5);
+  border-radius: 50%;
+  cursor: pointer;
+  transition: 350ms ease-in-out;
+}
+.avatar:hover::after {
+  opacity: 1;
+}
+.avatar:active::after {
+  opacity: 0.75;
+}
+.avatar {
+    box-sizing: border-box;
+    border: 4px solid silver;
+    border-radius: 50%;
+    overflow: hidden;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    {{ usuario }}
-    <h1>Hello!!</h1>
+  <div class="wreaper">
+    <avatar class="mb-5"/>
+    <div class="w-100">
+      <h2>{{ usuario.UserName }}</h2>
+      <p>{{ usuario.Email }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Avatar from '@/components/avatar.vue'
 
 export default {
   name: 'UserView',
   props: {
     usuario: null
   },
+  components: {
+    Avatar
+  }
 }
 </script>
+
+<style scoped>
+.wreaper {
+  display: grid;
+  grid-template-columns: 0 100%;
+  grid-gap: 11.5em;
+  align-items: center;
+}
+</style>

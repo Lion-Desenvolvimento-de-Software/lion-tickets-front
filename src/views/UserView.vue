190324bef86b39f10eec66b3cfe3822c76ab80a1
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-02-12 17:16:39 -0300

    Responsividade width, em desenvolvimento para mobile

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="wreaper">
+  <div class="wreaper h-100">
     <avatar class="mb-5"/>
     <div class="w-100">
       <h2>{{ usuario.UserName }}</h2>
       <p>{{ usuario.Email }}</p>
+      <div class="custom-layout-numbers-seguidores-and-curtidas">
+        <div class="d-grid">
+          <span class="custom-number">0</span>
+          <a>Seguidores</a>
+        </div>
+        <div class="d-grid">
+          <span class="custom-number">0</span>
+          <a>Seguindo</a>
+        </div>
+        <div class="d-grid">
+          <span class="custom-number">0</span>
+          <a>Curtidas</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,5 +42,20 @@ export default {
   grid-template-columns: 0 100%;
   grid-gap: 11.5em;
   align-items: center;
+  min-width: 835px;
 }
+
+.custom-layout-numbers-seguidores-and-curtidas {
+  display: flex;
+  justify-content: space-evenly;
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+@media screen and (max-width: 800px) {
+  .wreaper {
+    grid-gap: 6.5em;
+  }
+}
+
 </style>

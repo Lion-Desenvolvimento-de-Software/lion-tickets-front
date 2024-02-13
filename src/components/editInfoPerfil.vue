da0c99ba81c65bed069d25ba8797cee897770363
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-02-13 16:51:37 -0300

    span info inputs do perfil

--- src/components/editInfoPerfil.vue
+++ src/components/editInfoPerfil.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="m-4 d-grid justify-content-grid custom-form">
-    <input class="item1" type="text" name="telefone" :value="telefone" />
-    <input class="item2" type="text" name="telefone" :value="telefone" />
-    <input class="item3" type="text" name="telefone" :value="telefone" />
+  <div class="m-4 justify-content-grid custom-form">
+    <div class="d-flex align-items-center">
+      <span id="telefone">T</span>
+      <input id="telefone" type="text" name="telefone" :value="telefone" aria-describedby="telefone" />
+    </div>
+    <div class="d-flex align-items-center">
+      <span id="telefone">T</span>
+      <input type="date" name="telefone" :value="telefone" />
+    </div>
+    <div class="d-flex align-items-center">
+      <span id="telefone">T</span>
+      <select type="text" name="telefone">
+        <option :value="null">Selecione...</option>
+        <option value="">Masculino</option>
+        <option value="">Feminino</option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -20,11 +33,26 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   gap: 10px;
+  align-items: center;
 }
-.custom-form input {
+.custom-form span {
+  position: absolute;
+  padding-left: 0.8em;
+  padding-right: 0.8em;
+  top: 1px;
+  bottom: 1px;
+  border-right: 1px solid;
+}
+.custom-form > div {
+  position: relative;
+}
+.custom-form input, select {
+  width: 100%;
+  height: 26px;
   border-radius: 25px;
   border: 1px solid #000;
-  width: auto;
+  padding-left: 2.5em;
+  padding-right: 0.5em;
 }
 
 .custom-form input:hover {

4fde6e492223889134bbf97ee3c0c5b47e0e74b9
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-09-23 15:49:09 -0300

    Novo arquivo component, FooterComponent.vue

---
+++ src/components/footer-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+.footer {
+  grid-area: footer;
+  height: 250px;
+  background-color: rgb(58, 58, 58);
+}
+</style>

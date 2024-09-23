642d404a2db44a8b195b87052186f3c4bd41ddce
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-09-23 17:45:46 -0300

    Criando footer

--- src/components/footer-component.vue
+++ src/components/footer-component.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="footer">
-    
+    <table>
+      <tr>
+        <th>Sobre o</th>
+        <th>Minha Conta</th>
+      </tr>
+      <tr>
+        <td><span>LionTickets</span></td>
+        <td><span>Entrar</span></td>
+      </tr>
+      <tr>
+        <td><span>Compras</span></td>
+      </tr>
+      <tr>
+        <td><span>Carrinho</span></td>
+      </tr>
+      <tr>
+        <td><span>Cupom</span></td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FooterComponent'
+  name: 'FooterComponent',
 }
 </script>
 
@@ -15,5 +33,32 @@ export default {
   grid-area: footer;
   height: 250px;
   background-color: rgb(58, 58, 58);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 15px 15px 0 15px;
 }
+
+table {
+  text-align: start;
+}
+
+table tr th, table tr td {
+  padding: 0 25px;
+}
+
+table tr th {
+  padding-bottom: 15px;
+}
+
+table tr td {
+  color: rgba(255, 255, 255, 0.5);
+}
+
+  table tr td span:hover {
+    border: 0;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+    cursor: pointer;
+  }
 </style>

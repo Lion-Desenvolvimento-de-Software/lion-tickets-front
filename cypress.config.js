2479a64c69f616fe2b57155fe84cad3761424b3e
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-10-04 12:57:19 -0300

    add ngrok para teste

--- cypress.config.js
+++ cypress.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
       auth0_username: process.env.AUTH_USERNAME,
       auth0_password: process.env.AUTH0_PASSWORD,
       auth0_scope: process.env.REACT_APP_AUTH0_SCOPE,
-      auth0_domain: 'https://localhost:44360',
+      auth0_domain: 'https://f5ea-2804-7f0-aa38-ba-8829-1e84-1096-b60e.ngrok-free.app',
       auth0_client_id: process.env.REACT_APP_AUTH0_CLIENTID,
       auth0_client_secret: process.env.AUTH0_CLIENT_SECRET,
     },

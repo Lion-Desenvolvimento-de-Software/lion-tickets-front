da6e7343a50de21c8066f863a1a95d6e0bec91cb
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-10-08 17:49:49 -0300

    Criação da tela checkout e renomeado FormaPagamentoView

---
+++ src/views/Payment/CheckoutView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-fluid custom-checkout">
+    <div class="formas-pagamentos">
+      <tabela-forma-pagamento></tabela-forma-pagamento>
+    </div>
+    <div class="custom-info-checkout">
+      <h3>R$ </h3>
+      <button class="btn btn-success p-2">
+        <font-awesome-icon :icon="['fas', 'lock']" /> 
+        Realizar Pagamento
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabelaFormaPagamento from '@/components/Tabelas/TabelaFormaPagamento.vue';
+
+export default {
+  name: 'CheckoutView',
+  props: {
+    itens: null
+  },
+  components: {
+    TabelaFormaPagamento
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.custom-checkout {
+  height: 100vh;
+  padding: 80px;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+}
+
+.formas-pagamentos {
+  width: 100%;
+  padding: 15px;
+  font-size: 40px;
+}
+</style>

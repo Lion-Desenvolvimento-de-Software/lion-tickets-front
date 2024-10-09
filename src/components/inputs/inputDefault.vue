9e70a692d17adb727eb6637f8af1f9486e0afa2d
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-10-09 18:24:47 -0300

    novo componente inputDefault e checkout com formulário da forma de pagamento

---
+++ src/components/inputs/inputDefault.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="position-relative p-2">
+    <input type="text" :id="id" :name="name" :placeholder="placeholder" >
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inputDefault',
+  props: {
+    id: null,
+    name: null,
+    placeholder: null,
+    label: null
+  }
+}
+</script>
+
+<style scoped>
+input {
+  position: relative;
+  width: 100%;
+  height: 30px;
+  border-radius: 15px;
+  padding: 0 8px;
+  border: 1px solid;
+}
+
+label {
+  position: absolute;
+  left: 25px;
+  top: 0;
+  font-size: 11px;
+  background-color: white;
+  transition: .5s ease-out;
+}
+
+/* input:focus ~ label {
+  background-color: transparent;
+  top: -10px;
+} */
+</style>
--- src/views/Payment/CheckoutView.vue
+++ src/views/Payment/CheckoutView.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="container-fluid custom-checkout">
     <div class="formas-pagamentos">
-      <tabela-forma-pagamento></tabela-forma-pagamento>
+      <tabela-forma-pagamento @formSelected="formPaymentAdd"></tabela-forma-pagamento>
     </div>
-    <div class="custom-info-checkout">
+    <div class="custom-formulario-checkout h-50 w-100" v-if="formPaymentId">
+      <div class="row">
+        <div class="col">
+          <input-default id="cpf" name="CPF" placeholder="Informe seu CPF" label="CPF"></input-default>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <input-default id="fullName" name="FullName" placeholder="Informe seu nome completo" label="Nome Completo"></input-default>
+        </div>
+      </div>
+      <div class="row" v-if="[1, 2].includes(formPaymentId)">
+        <div class="col-4">
+          <input-default id="expirationDate" name="ExpirationDate" placeholder="01/01/2000" label="Data de validade"></input-default>
+        </div>
+        <div class="col-6">
+          <input-default id="numberCard" name="NumberCard" placeholder="xxxx-xxxx-xxxx-xxxx" label="Número do cartão"></input-default>
+        </div>
+        <div class="col-2">
+          <input-default id="cvv" name="CVV" placeholder="111" label="CVV"></input-default>
+        </div>
+      </div>
+    </div>
+    <div class="custom-finalization-checkout">
       <h3>R$ </h3>
       <button class="btn btn-success p-2">
         <font-awesome-icon :icon="['fas', 'lock']" /> 
@@ -15,14 +38,26 @@
 
 <script>
 import TabelaFormaPagamento from '@/components/Tabelas/TabelaFormaPagamento.vue';
+import inputDefault from '@/components/inputs/inputDefault.vue';
 
 export default {
   name: 'CheckoutView',
+  data() {
+    return {
+      formPaymentId: null
+    }
+  },
   props: {
     itens: null
   },
   components: {
-    TabelaFormaPagamento
+    TabelaFormaPagamento,
+    inputDefault
+  },
+  methods: {
+    formPaymentAdd(item) {
+      this.formPaymentId = item?.id;
+    }
   }
 }
 </script>
@@ -37,14 +72,29 @@ export default {
   height: 100vh;
   padding: 80px;
   display: grid;
-  grid-template-rows: 1fr;
+  grid-template-rows: 1fr 1fr;
   grid-template-columns: 1fr 1fr;
   justify-items: center;
+  grid-template-areas: 
+  "form-payment        formulary"
+  "form-payment        formulary"
+  "form-payment        finalization";
 }
 
 .formas-pagamentos {
+  grid-area: form-payment;
   width: 100%;
   padding: 15px;
   font-size: 40px;
+}
+
+.custom-formulario-checkout {
+  grid-area: formulary;
+  padding: 15px;
+  height: auto;
+}
+
+.custom-finalization-checkout {
+  grid-area: finalization;
 }
 </style>

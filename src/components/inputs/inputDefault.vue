6d62dcd4016d384c85711dfdd9a98f9226a7b5e2
Maurício Ramos <mauricio2017rubiale@gmail.com>
2024-10-10 18:15:06 -0300

    v-model funcionando para o componente pai usando um emit com 'update:data'

--- src/components/inputs/inputDefault.vue
+++ src/components/inputs/inputDefault.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="position-relative p-2">
-    <input type="text" :id="id" :name="name" :placeholder="placeholder" >
+    <input :value="data" @input="$emit('update:data', $event.target.value)" :disabled="isDisabled" type="text" :id="id" :name="name" :placeholder="placeholder" >
     <label :for="id">{{ label }}</label>
   </div>
 </template>
 
 <script>
 export default {
+  emits: ['update:data'],
   name: 'inputDefault',
   props: {
     id: null,
     name: null,
     placeholder: null,
-    label: null
+    label: null,
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    data: null
   }
 }
 </script>
@@ -21,7 +27,7 @@ export default {
 input {
   position: relative;
   width: 100%;
-  height: 30px;
+  height: 40px;
   border-radius: 15px;
   padding: 0 8px;
   border: 1px solid;
@@ -34,6 +40,10 @@ label {
   font-size: 11px;
   background-color: white;
   transition: .5s ease-out;
+}
+
+input:disabled {
+  background-color: rgba(193, 193, 193, 0.5);
 }
 
 /* input:focus ~ label {
